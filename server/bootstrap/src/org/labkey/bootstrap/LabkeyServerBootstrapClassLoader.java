912974077c823d7c884f8a8fa4e162d3e13ad067
Josh Eckels <jeckels@labkey.com>
2014-10-27 22:35:47 +0000

    Correctly target logs directory when catalina.base and catalina.home point at different locations
    SVN r35025 |2014-10-27 22:35:47 +0000

--- server/bootstrap/src/org/labkey/bootstrap/LabkeyServerBootstrapClassLoader.java
+++ server/bootstrap/src/org/labkey/bootstrap/LabkeyServerBootstrapClassLoader.java
@@ -41,7 +41,10 @@ public class LabkeyServerBootstrapClassLoader extends WebappClassLoader
         String headless = "java.awt.headless";
         if (System.getProperty(headless) == null)
             System.setProperty(headless, "true");
-        PipelineBootstrapConfig.ensureLogHomeSet(System.getProperty("catalina.home") + "/logs");
+        // On most installs, catalina.home and catalina.base point to the same directory. However, it's possible
+        // to have multiple instances share the Tomcat binaries but have their own ./logs, ./conf, etc directories
+        // Thus, we want to use catalina.base for our place to find log files. http://www.jguru.com/faq/view.jsp?EID=1121565
+        PipelineBootstrapConfig.ensureLogHomeSet(System.getProperty("catalina.base") + "/logs");
     }
 
     private ModuleExtractor _moduleExtractor;

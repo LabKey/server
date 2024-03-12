1f2928df432a2ed92a489e41419ec786dc60f9ba
labkey-tchad <tchad@labkey.com>
2024-03-12 12:56:55 -0700

    Update files included with embedded distributions

--- server/configs/webapps/embedded/install_service.bat
+++ server/configs/webapps/embedded/install_service.bat
@@ -1,10 +1,11 @@
 set LABKEY_HOME=C:\labkey\labkey
-set JAVA_HOME=C:\labkey\apps\java\jdk-17.0.9+9
+set LABKEY_APPS=C:\labkey\apps
+set JAVA_HOME=%LABKEY_HOME%\java\jdk-17.0.9+9
 
-prunsrv.exe //IS//tc10embedded ^
-    --DisplayName "LabKey Tomcat 10 Embedded - tc10embedded" ^
-    --Description "LabKey Tomcat 10 Embedded" ^
-    --Install "%LABKEY_HOME%\prunsrv.exe" ^
+prunsrv.exe //IS//labkeyServer ^
+    --DisplayName "LabKey Server - labkeyServer" ^
+    --Description "LabKey Server Scientific Data Management System" ^
+    --Install "%LABKEY_APPS%\prunsrv.exe" ^
     --LogPath "%LABKEY_HOME%\logs" ^
     --StdOutput auto ^
     --StdError auto ^
@@ -22,7 +23,7 @@ prunsrv.exe //IS//tc10embedded ^
     --StopTimeout 60 ^
     --Startup manual ^
     --LogLevel Debug ^
-    --JvmOptions "-Djava.io.tmpdir=%LABKEY_HOME%\tomcat-tmp;-XX:+HeapDumpOnOutOfMemoryError;-XX:HeapDumpPath=%LABKEY_HOME%\tomcat-tmp;-DterminateOnStartupFailure=true;%JvmArgs%" ^
+    --JvmOptions "-Djava.io.tmpdir=%LABKEY_HOME%\labkey-tmp;-XX:+HeapDumpOnOutOfMemoryError;-XX:HeapDumpPath=%LABKEY_HOME%\labkey-tmp;-DterminateOnStartupFailure=true;%JvmArgs%" ^
     --JvmOptions9 "--add-opens=java.base/java.lang=ALL-UNNAMED#--add-opens=java.base/java.io=ALL-UNNAMED#--add-opens=java.base/java.util=ALL-UNNAMED#--add-opens=java.base/java.util.concurrent=ALL-UNNAMED#--add-opens=java.rmi/sun.rmi.transport=ALL-UNNAMED" ^
     --JvmMs 2048 ^
     --JvmMx 2048

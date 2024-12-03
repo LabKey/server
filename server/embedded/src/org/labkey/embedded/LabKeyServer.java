0ad744cdbb51ce146ae8755cc28dd0a6b8c8272a
Adam Rauch <adam@labkey.com>
2024-12-02 19:33:09 -0800

    Slim down embedded dependencies (#937)

--- server/embedded/src/org/labkey/embedded/LabKeyServer.java
+++ server/embedded/src/org/labkey/embedded/LabKeyServer.java
@@ -1,6 +1,5 @@
 package org.labkey.embedded;
 
-import jakarta.validation.constraints.NotNull;
 import org.apache.catalina.connector.Connector;
 import org.labkey.bootstrap.PipelineBootstrapConfig;
 import org.springframework.boot.Banner;
@@ -405,7 +404,6 @@ public class LabKeyServer
 
         private String webAppLocation;
         private String workDirLocation;
-        @NotNull (message = "Must provide encryptionKey")
         private String encryptionKey;
         private String oldEncryptionKey;
         private String legacyContextPath;
@@ -499,6 +497,8 @@ public class LabKeyServer
 
         public String getEncryptionKey()
         {
+            if (null == encryptionKey)
+                throw new RuntimeException("Must provide encryptionKey");
             return encryptionKey;
         }
 

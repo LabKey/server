0943174b5cbd891be635a663dd93ad574cbc36e1
ian <ians@labkey.com>
2025-02-11 08:49:38 -0800

    Clean up class loader

--- server/bootstrap/src/org/labkey/bootstrap/LabKeyBootstrapClassLoader.java
+++ server/bootstrap/src/org/labkey/bootstrap/LabKeyBootstrapClassLoader.java
@@ -55,6 +55,8 @@ public class LabKeyBootstrapClassLoader extends WebappClassLoader implements Exp
         String headless = "java.awt.headless";
         if (System.getProperty(headless) == null)
             System.setProperty(headless, "true");
+
+        // Initialize the Log4j Composite Configuration factory
         System.setProperty("log4j.configurationFactory", "org.labkey.api.util.logging.LabKeyLog4j2ConfigurationFactory");
     }
 
@@ -384,29 +386,4 @@ public class LabKeyBootstrapClassLoader extends WebappClassLoader implements Exp
 
         return deleted;
     }
-//
-//
-//    @Override
-//    public Class<?> loadClass(String name, boolean resolve) throws ClassNotFoundException
-//    {
-//        if (name.contains("LabKeyLog4j2ConfigurationFactory"))
-//        {
-//            return findSystemClass(name);
-////            return null;
-//        }
-//
-//        return super.loadClass(name, resolve);
-//    }
-//
-//    @Override
-//    public Class<?> findClass(String name) throws ClassNotFoundException {
-//        if (name.contains("LabKeyLog4j2ConfigurationFactory"))
-//        {
-//            return findSystemClass(name);
-////            return null;
-//        }
-//
-//        return super.findClass(name);
-//    }
-
 }

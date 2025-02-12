589867c79621ee019f54ae858e11ab2fb0e23c23
ian <ians@labkey.com>
2025-02-11 16:27:36 -0800

    Clean up and add comments

--- server/embedded/src/org/labkey/embedded/LabKeySpringBootClassLoader.java
+++ server/embedded/src/org/labkey/embedded/LabKeySpringBootClassLoader.java
@@ -3,8 +3,6 @@ package org.labkey.embedded;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.labkey.bootstrap.LabKeyBootstrapClassLoader;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
 import java.io.IOException;
 import java.io.InputStream;

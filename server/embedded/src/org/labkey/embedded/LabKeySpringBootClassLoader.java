0387152a4d6e8a2bed852b7fbf8c86df8af4bea9
labkey-jeckels <jeckels@labkey.com>
2025-06-02 13:56:22 -0700

    Fix Log4J initialization without Spring Boot dev tools

--- server/embedded/src/org/labkey/embedded/LabKeySpringBootClassLoader.java
+++ server/embedded/src/org/labkey/embedded/LabKeySpringBootClassLoader.java
@@ -65,11 +65,11 @@ public class LabKeySpringBootClassLoader extends LabKeyBootstrapClassLoader
             ClassLoader parent = getParent();
             while (parent != null)
             {
-                LOG.info("Looking for SessionAppending - checking ClassLoader " + parent);
+                LOG.debug("Looking for SessionAppending - checking ClassLoader " + parent);
                 if (parent.getClass().getName().equals("jdk.internal.loader.ClassLoaders$AppClassLoader") ||
                         parent.getClass().getName().equals("org.springframework.boot.loader.launch.LaunchedClassLoader"))
                 {
-                    return getParent().getParent().loadClass(name);
+                    return parent.loadClass(name);
                 }
                 parent = parent.getParent();
             }

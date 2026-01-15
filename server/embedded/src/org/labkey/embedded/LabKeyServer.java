ffa7d7e1dd5c213da29598faa6ea9fae8ec591a9
labkey-jeckels <jeckels@labkey.com>
2026-01-14 17:16:13 -0800

    Upgrade to Spring Boot 4, Spring Framework 7, and Tomcat 11

--- server/embedded/src/org/labkey/embedded/LabKeyServer.java
+++ server/embedded/src/org/labkey/embedded/LabKeyServer.java
@@ -7,7 +7,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.ApplicationPidFileWriter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.tomcat.servlet.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -193,7 +193,7 @@ public class LabKeyServer
             Connector httpConnector = new Connector();
             httpConnector.setScheme("http");
             httpConnector.setPort(contextProperties.getHttpPort());
-            result.addAdditionalTomcatConnectors(httpConnector);
+            result.addAdditionalConnectors(httpConnector);
         }
 
         return result;

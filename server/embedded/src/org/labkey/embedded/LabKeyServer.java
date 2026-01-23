8cc1e122863c861758c3ee574a6109a58563f15c
labkey-jeckels <jeckels@labkey.com>
2026-01-22 20:24:17 -0800

    GitHub Issue 796: JSON logging stopped after Tomcat/Spring update

--- server/embedded/src/org/labkey/embedded/LabKeyServer.java
+++ server/embedded/src/org/labkey/embedded/LabKeyServer.java
@@ -92,6 +92,32 @@ public class LabKeyServer
 
         application.setDefaultProperties(new HashMap<>()
              {{
+                // GitHub Issue 796: JSON logging stopped after Tomcat/Spring update
+                 // Propagate log4j configuration to Spring Boot config, which is necessary with Spring Boot 4.x
+                 String log4JConfig = System.getProperty("log4j.configurationFile");
+                 if (log4JConfig != null)
+                 {
+                     String[] log4JConfigParts = log4JConfig.split(",");
+                     if (log4JConfigParts.length > 0)
+                     {
+                         if ("log4j2.xml".equals(log4JConfigParts[0]))
+                         {
+                             // Assume this is the one packaged with our embedded build and on the classpath
+                             put("logging.config", "classpath:log4j2.xml");
+                         }
+                         else
+                         {
+                             put("logging.config", log4JConfigParts[0]);
+                         }
+                         if (log4JConfigParts.length == 2)
+                         {
+                            put("logging.log4j2.config.override", log4JConfigParts[1]);
+                         }
+                         else
+                             throw new IllegalArgumentException("log4j.configurationFile must be in the form log4j2.xml[,secondaryFile]");
+                     }
+                 }
+
                  put("server.tomcat.basedir", ".");
                  put("server.tomcat.accesslog.directory", logHome);
 
